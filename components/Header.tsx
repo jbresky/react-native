62ef143adf4430d905fe6661ccf1a8ef2f0d7127
jbresky <joaquin_bresky@hotmail.com>
2024-01-17 17:25:01 -0300

    improving styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const Header = () => {
-    return ( 
-        <View style={{ flex: 0.05, paddingTop: 60, paddingHorizontal: 20 ,backgroundColor: 'black'}}>
-            <Text style={{ color: 'white', fontSize: 24}}>
-                Welcome to Lemon Restaurant
+    return (
+        <View style={headerStyles.container}>
+            <Text style={headerStyles.leftText}>
+                AC
+            </Text>
+            <Text style={headerStyles.rightText}>
+                USDc Account
             </Text>
         </View>
-     );
+    );
 }
- 
+
+const headerStyles = StyleSheet.create({
+    container: {
+        flex: 0.05,
+        flexDirection: 'row',
+        gap: 10,
+        paddingTop: 60,
+        paddingHorizontal: 20,
+        backgroundColor: '#f6f6f6'
+    },
+    leftText: {
+        color: '#58c799',
+        fontSize: 18,
+    },
+    rightText: {
+        color: '#000',
+        fontSize: 18
+    }
+})
+
 export default Header;
